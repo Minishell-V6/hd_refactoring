0752545aa5f0b507839e458bd20e30d87186d6f0
hey-chocopie <ghdyd6372@naver.com>
2021-07-10 11:34:48 +0900

    fix_holiday

--- srcs/execute/ft_unset.c
+++ srcs/execute/ft_unset.c
@@ -21,7 +21,7 @@ int				ft_find_envkey(char *key, char **envp)
 	key_len = ft_strlen(key);
 	while (envp[i])
 	{
-		if (ft_strncmp(key, envp[i], key_len) == 0 && envp[i][key_len] == '=')
+		if (ft_strncmp(key, envp[i], key_len) == 0 && (envp[i][key_len] == '=' || envp[i][key_len] == 0))
 			return (i);
 		i++;
 	}
